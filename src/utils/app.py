33486f63fe7fa0dd7869f07d671440f9a4e5a101
JairRamosG <ramosgonzalezjair@gmail.com>
2026-02-21 19:40:45 -0600

    requerimientos

---
+++ src/utils/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard COVID-19 Comorbilidades",
+    page_icon="",
+    layout="wide")
+
+
+st.title("🦠 Dashboard Interactivo de COVID-19")
+st.markdown("---")
